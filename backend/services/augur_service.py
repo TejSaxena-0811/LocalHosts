a10d73ec3560df4b1077231ca7239b94d7edd349
Omm_Routray <93387363+ighomm@users.noreply.github.com>
2026-04-03 18:38:20 +0530

    Update augur_service.py

--- backend/services/augur_service.py
+++ backend/services/augur_service.py
@@ -1,4 +1,4 @@
 from augurai import augur
-
+# Wrapper function to call Augur AI and generate threat analysis using product info and diagram
 def augur_service(product_info, product_diagram, prompts_file, max_tokens=4000):
     return augur(product_info, product_diagram, prompts_file, max_tokens)
